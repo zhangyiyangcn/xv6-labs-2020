5bbf25e40de8592fef72d4843fecd7395c79b270
zhangyiyang <zhangyiyangcn@163.com>
2024-06-20 18:41:20 +0800

    sleep、pingpong、primes

---
+++ user/sleep.c
@@ -0,0 +1,26 @@
+// Lab Xv6 and Unix utilities
+// sleep.c
+
+// 引入声明类型的头文件
+#include "kernel/types.h"
+// 引入声明系统调用和 user/ulib.c 中其他函数的头文件
+#include "user/user.h"
+
+// int main(int argc,char* argv[])
+// argc 是命令行总的参数个数  
+// argv[] 是 argc 个参数，其中第 0 个参数是程序的全名，以后的参数是命令行后面跟的用户输入的参数
+int main(int argc, char *argv[])
+{
+    // 如果命令行参数不等于2个，则打印错误信息
+    if (argc != 2)
+    {
+        printf("Sleep time \n");
+        exit(1);
+    }
+    // 把字符串型参数转换为整型
+    int time = atoi(argv[1]);
+    // 调用系统调用 sleep 函数，传入整型参数
+    sleep(time);
+    // 正常退出程序
+    exit(0);
+}
